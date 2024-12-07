1702e2fcead285d8cd56d617b9abe527b7a3930c
Hüma <huma.karapinar@gmail.com>
2024-12-07 14:30:43 +0300

    Adjusting movie diary

--- src/main/java/com/movieshed/gui/profile/MovieDiaryPanel.java
+++ src/main/java/com/movieshed/gui/profile/MovieDiaryPanel.java
@@ -24,21 +24,32 @@ public class MovieDiaryPanel extends JPanel {
         this.registerPanel = registerPanel;
         setLayout(new BorderLayout());
 
+        setBackground(Color.BLACK);
+
         MovieShedUser currentUser = registerPanel.getCurrentUser();
         if (currentUser != null) {
             userNameLabel = new JLabel("Welcome, " + currentUser.getUserName());
             userNameLabel.setFont(new Font("Arial", Font.BOLD, 16));
             userNameLabel.setHorizontalAlignment(SwingConstants.LEFT);
+            userNameLabel.setForeground(Color.WHITE);
         } else {
             userNameLabel = new JLabel("No user logged in");
+            userNameLabel.setForeground(Color.WHITE);
         }
 
         diaryListModel = new DefaultListModel<>();
         diaryList = new JList<>(diaryListModel);
+        diaryList.setBackground(Color.BLACK);
+        diaryList.setForeground(Color.WHITE);
         JScrollPane diaryScroll = new JScrollPane(diaryList);
 
         notesArea = new JTextArea(5, 20);
+        notesArea.setBackground(Color.BLACK);
+        notesArea.setForeground(Color.WHITE);
+
         saveButton = new JButton("Save Notes");
+        saveButton.setBackground(Color.WHITE);
+        saveButton.setForeground(Color.BLACK);
 
         add(userNameLabel, BorderLayout.NORTH);
         add(diaryScroll, BorderLayout.CENTER);
